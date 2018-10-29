83990866413d15a10b228c540244c795bb1e245a
alexandrurizea86 <alexandrurizea86@users.noreply.github.com>
2018-10-29 15:20:10 +0200

    First Commit

---
+++ src/app/Program.java
@@ -0,0 +1,36 @@
+package app;
+
+import java.util.Scanner;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Integer a;
+	    Float b;
+	    String s;
+
+	      Scanner in = new Scanner(System.in);
+
+	      System.out.println("Introdu cuvantul 'ALEX' ");
+	      s = in.nextLine();
+	      System.out.println("Ai introdus cuvantul corect! ");
+	      
+	  
+	      
+	      
+	      
+	      System.out.println("Introdu numarul '860'");
+	      a = in.nextInt();
+	      System.out.println("Ai introdus numarul corect!");
+	      
+	    
+	    	  
+	      System.out.println("Introdu valoarea lui 'pi'");
+	      b = in.nextFloat();
+	      System.out.println("Ai introdus valoarea corecta!");   
+		
+		
+	}
+
+}
